8fcc1ceac574a28fc7a2c9a5e7cf524360c57399
Anderson-Silva1 <anderson.developer360@gmail.com>
2025-11-13 18:36:13 -0300

    feat: models transaction e contsntes

---
+++ src/constants/index.ts
@@ -0,0 +1,48 @@
+import {
+  CategoryCategory,
+  Status,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@/model/transaction";
+
+export const PAYMENT_METHOD_LABELS: Record<TransactionPaymentMethod, string> = {
+  credit_card: "Cartão de Crédito",
+  debit_card: "Cartão de Débito",
+  pix: "Pix",
+  cash: "Dinheiro",
+  boleto: "Boleto",
+  bank_transfer: "Transferência Bancária",
+  meal_voucher: "Vale Refeição / Alimentação",
+  other: "Outros",
+};
+
+export const STATUS_LABELS: Record<Status, string> = {
+  pending: "Pendente",
+  completed: "Efetivado", // ou "Pago" / "Recebido" dependendo do contexto
+  cancelled: "Cancelado",
+};
+
+export const CATEGORY_LABELS: Record<CategoryCategory, string> = {
+  // Receitas
+  salary: "Salário",
+  freelance: "Freelance / Extra",
+  investment: "Rendimentos",
+  gift: "Presente / Doação",
+
+  // Despesas
+  housing: "Moradia",
+  utilities: "Contas (Luz/Água/Net)",
+  food: "Alimentação",
+  transport: "Transporte",
+  health: "Saúde",
+  education: "Educação",
+  leisure: "Lazer",
+  shopping: "Compras",
+  services: "Serviços / Assinaturas",
+  other: "Outros",
+};
+
+export const TRANSACTION_TYPE_LABELS: Record<TransactionType, string> = {
+  revenue: "Receita",
+  expense: "Despesa",
+};
--- src/model/transaction.ts
+++ src/model/transaction.ts
@@ -1,12 +1,49 @@
-interface TransactionModel {
+export interface TransactionModel {
   id: string;
   title: string;
   description?: string;
   centsAmount: number; // Valor em centavos
-  category: "salario" | "lanche";
-  type: "revenue" | "expense";
+  category: CategoryCategory;
+  type: TransactionType;
   createdAt: string;
   updatedAt: string;
-  status: "pending" | "completed" | "cancelled";
-  paymentMethod: "credit_card" | "debit_card" | "pix" | "cash" | "transfer";
+  status: Status;
+  paymentMethod: TransactionPaymentMethod;
 }
+
+export type Status = "pending" | "completed" | "cancelled";
+
+export type TransactionPaymentMethod =
+  | "credit_card"
+  | "debit_card"
+  | "pix"
+  | "cash"
+  | "boleto"
+  | "bank_transfer"
+  | "meal_voucher" // O famoso Vale Refeição/Alimentação
+  | "other";
+
+export type TransactionType = "revenue" | "expense";
+
+export type CategoryCategory =
+  // Receitas
+  | "salary"
+  | "freelance"
+  | "investment"
+  | "gift"
+
+  // Despesas Essenciais
+  | "housing" // Aluguel, Condomínio
+  | "utilities" // Luz, Água, Internet
+  | "food" // Mercado, Restaurante (antigo 'lanche')
+  | "transport" // Uber, Gasolina, Ônibus
+  | "health" // Farmácia, Plano de Saúde
+  | "education" // Faculdade, Cursos
+
+  // Estilo de Vida
+  | "leisure" // Cinema, Viagem
+  | "shopping" // Roupas, Eletrônicos
+  | "services" // Assinaturas (Netflix, Spotify)
+
+  // Outros
+  | "other";
